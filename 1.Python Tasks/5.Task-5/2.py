58f982ab28548558c223c0a633f08937f122b674
Sutesh <75980293+sutesh-latchubuktha@users.noreply.github.com>
2022-08-04 10:01:35 +0530

    Add files via upload

---
+++ 1.Python Tasks/5.Task-5/2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        result = 0
+        
+        while x > 0 or y > 0 :
+            if (x&1) ^ (y &1) == 1:
+                result += 1
+            x >>= 1
+            y >>= 1 
+        return result
+        
